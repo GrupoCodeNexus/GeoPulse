3ded3de5704764cea4bc3ffe32566cf239ae82bd
Francisco <franciscovargasmarcal@gmail.com>
2025-05-28 15:59:45 -0300

    Feat: estruturando os imports e rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './Routes/Home'
+import Error from './Routes/Error'
+import Sobre from './Routes/Sobre'
+import Solucao from './Routes/Solucao'
+import Login from './Routes/Login'
+import Cadastrar from './Routes/Cadastrar'
+import Nav from './Components/Nav'
+import Footer from './Components/Footer'
 
 function App() {
 
   return (
     <>
+    <Router>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="*" element={<Error/>}/>
+        <Route path="/sobre" element={<Sobre/>}/>
+        <Route path="/solucao" element={<Solucao/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/cadastrar" element={<Cadastrar/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
     </>
   )
 }
